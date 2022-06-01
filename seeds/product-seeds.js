1086824082c379eeb4f2e1e7c8fa4cc4506e5a25
Brainybrian316 <brainybrian316@gmail.com>
2022-06-01 18:33:45 -0400

    testing

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,34 +2,94 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
+    product_name: 'Andes Mints',
     price: 14.99,
     stock: 14,
     category_id: 1,
   },
   {
-    product_name: 'Running Sneakers',
+    product_name: 'Appleheads',
     price: 90.0,
     stock: 25,
-    category_id: 5,
+    category_id: 1,
   },
   {
-    product_name: 'Branded Baseball Hat',
+    product_name: 'Astronaut Ice Cream Sandwich',
     price: 22.99,
     stock: 12,
-    category_id: 4,
+    category_id: 1,
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
+    product_name: 'Blow Pops',
+    price: 12.99,
+    stock: 50,
+    category_id: 1,
+  },
+  {
+    product_name: 'BuckEyes',
+    price: 29.99,
+    stock: 22,
+    category_id: 1,
+  },
+  {
+    product_name: 'BarNone',
+    price: 14.99,
+    stock: 14,
+    category_id: 2,
+  },
+  {
+    product_name: 'Nerds',
+    price: 90.0,
+    stock: 25,
+    category_id: 2,
+  },
+  {
+    product_name: 'Spree',
+    price: 22.99,
+    stock: 12,
+    category_id: 2,
+  },
+  {
+    product_name: 'Skor',
+    price: 12.99,
+    stock: 50,
+    category_id: 2,
+  },
+  {
+    product_name: 'Cow Tales',
+    price: 29.99,
+    stock: 22,
+    category_id: 2,
+  },
+  {
+    product_name: 'Baby Bottle Pops',
+    price: 14.99,
+    stock: 14,
+    category_id: 3,
+  },
+  {
+    product_name: 'Color Your Mouth Gumballs',
+    price: 90.0,
+    stock: 25,
+    category_id: 3,
+  },
+  {
+    product_name: 'Creme Savers',
+    price: 22.99,
+    stock: 12,
+    category_id: 3,
+  },
+  {
+    product_name: 'Gummi Hot Dog',
     price: 12.99,
     stock: 50,
     category_id: 3,
   },
   {
-    product_name: 'Cargo Shorts',
+    product_name: 'Nerds Rope',
     price: 29.99,
     stock: 22,
-    category_id: 2,
+    category_id: 3,
   },
 ];
 
